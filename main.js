e8e61fa347296a6d33b105f541267ebf4254ef7c
Pearlpatel <81533030+Pearlpatel@users.noreply.github.com>
2022-01-26 19:13:39 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function play() {
+    window.location="play.html";
+}
+function dining() {
+    window.location="dining.html";
+}
+function bed() {
+    window.location="bed.html";
+}
+function kitchen() {
+    window.location="kitchen.html";
+}
